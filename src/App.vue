7f29dffca570b538868f869728d9f8bedc722c98
Ashley Miller <comedy.writer.ash@gmail.com>
2018-12-11 16:05:29 +0000

    finally got something working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,130 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="section">
+      <div class="container">
+        <div v-if="!this.accessToken" class="notification is-warning">
+          Enter your client ID below.
+        </div>
+        <div class="columns">
+          <div v-if="!this.accessToken" class="column">
+            <nav class="panel">
+              <div class="panel-block">
+                <p class="control has-icons-left">
+                  <input v-model="clientId" class="input" type="text" placeholder="Client ID here">
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-key"></i>
+                  </span>
+                </p>
+              </div>
+              <div class="panel-block">
+                <button v-on:click="getToken()" class="button is-link is-outlined is-fullwidth">
+                  Click to Authenticate
+                </button>
+              </div>
+            </nav>
+          </div>
+          <div class="column">
+            <nav class="panel">
+              <p class="panel-heading">
+                Search Artists
+              </p>
+              <div class="panel-block">
+                <p class="control has-icons-left">
+                  <input v-model="artist" @keyup.enter="getArtist" class="input" type="text" placeholder="Search for your faves">
+                </p>
+              </div>
+
+              <div class="track-listing">
+              <a v-for="track in tracks" class="panel-block is-active">
+                  {{ track.name }}
+                  {{ track.artists[0].name }}
+                  <audio
+                    :src="track.preview_url"
+                    controls>
+                    Your browser does not support the <code>audio</code> element.
+                  </audio>
+              </a>
+            </div>
+            </nav>
+          </div>
+        </div>  
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Axios from 'axios'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      clientId: '',
+      accessToken: this.getParameterByName('access_token'),
+      config: [],
+      artist: '',
+      data: [],
+      tracks: []
+    }
+  },
+  methods: {
+    getToken: function(){
+      window.location = 'https://accounts.spotify.com/authorize?client_id=' + this.clientId + '&redirect_uri=http://localhost:8080&response_type=token';
+    },
+    getParameterByName: function(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?#]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+    },
+    getArtist: function(){
+      this.data = '';
+      this.tracks = '';
+      this.config = {headers: {'Authorization': 'Bearer ' + this.accessToken}};
+      Axios.get('https://api.spotify.com/v1/search?q=' + this.artist + '&type=track,artist&market=US', this.config)
+        .then(response => {
+          this.data = response.data;
+          this.tracks = response.data.tracks.items;
+        })
+        .catch(e => {
+          console.log(error);
+        })
+    }
   }
-}
+};
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .container {
+    text-align: center;
+    font-size: 1.5em;
+  }
+  input.input {
+      padding: 1em;
+      width: 20%;
+  }
+
+  track-listing {
+      text-align: left;
+      width: 75%;
+      margin: 0 auto;
+  }
+
+  button.button.is-link.is-outlined.is-fullwidth {
+      background: blue;
+      color: white;
+      padding: 1em;
+      border: none;
+  }
+
+  a.panel-block.is-active {
+      width: 100%;
+      float: left;
+      padding: 1em 0;
+  }
 </style>
